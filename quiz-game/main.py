eb9203a00afc5af70b608f727cdad1e767937618
= <jackson.m.player@gmail.com>
2021-06-15 21:08:35 -0700

    Add quiz game

---
+++ quiz-game/main.py
@@ -0,0 +1,12 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data["results"]:
+    new_q = Question(question["question"], question["correct_answer"])
+    question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
---
+++ quiz-game/quiz_brain.py
@@ -0,0 +1,35 @@
+class QuizBrain:
+    def __init__(self, questions, question_number=0):
+        self.questions = questions
+        self.question_number = question_number
+        self.score = 0
+
+    def next_question(self):
+        question_text = self.questions[self.question_number].text
+        user_answer = input(f'Q.{self.question_number + 1}: {question_text} (True / False)? ')
+        self.check_answer(user_answer)
+        self.question_number += 1
+        print("\n")
+        if len(self.questions) == self.question_number:
+            self.print_final_score()
+
+    def print_score(self):
+        print(f"Your score is: {self.score} / {self.question_number + 1}")
+
+    def check_answer(self, user_answer):
+        question_answer = self.questions[self.question_number].answer
+        if user_answer.lower() == question_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("You got it wrong!")
+        print(f"The correct answer was {question_answer}")
+        self.print_score()
+
+    def still_has_questions(self):
+        return self.question_number + 1 <= len(self.questions)
+
+    def print_final_score(self):
+        print("\n")
+        print("You completed the quiz.")
+        print(f"Your final score is: {self.score}")
